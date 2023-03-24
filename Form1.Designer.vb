177a4df1b63518acc627b4456e073d96bfb24132
michelle1574 <michelle1574@google.com>
2023-03-24 23:46:59 +0100

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,307 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox2 = New TextBox()
+        Label3 = New Label()
+        GroupBox1 = New GroupBox()
+        RadioButton4 = New RadioButton()
+        RadioButton3 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        RadioButton1 = New RadioButton()
+        GroupBox2 = New GroupBox()
+        RadioButton10 = New RadioButton()
+        RadioButton9 = New RadioButton()
+        RadioButton8 = New RadioButton()
+        RadioButton7 = New RadioButton()
+        RadioButton6 = New RadioButton()
+        RadioButton5 = New RadioButton()
+        Button1 = New Button()
+        CheckBox1 = New CheckBox()
+        LinkLabel1 = New LinkLabel()
+        LinkLabel2 = New LinkLabel()
+        GroupBox1.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(47, 66)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(580, 23)
+        TextBox1.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(12, 69)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(29, 15)
+        Label1.TabIndex = 1
+        Label1.Text = "Title"' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("GPP Text", 27.7499962F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.Location = New Point(71, 9)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(517, 51)
+        Label2.TabIndex = 2
+        Label2.Text = "Michelle1574's Error Creator"' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(47, 95)
+        TextBox2.Multiline = True
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(580, 265)
+        TextBox2.TabIndex = 3
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(12, 98)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(28, 15)
+        Label3.TabIndex = 4
+        Label3.Text = "Text"' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(RadioButton4)
+        GroupBox1.Controls.Add(RadioButton3)
+        GroupBox1.Controls.Add(RadioButton2)
+        GroupBox1.Controls.Add(RadioButton1)
+        GroupBox1.Location = New Point(47, 366)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(98, 121)
+        GroupBox1.TabIndex = 5
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Icon"' 
+        ' RadioButton4
+        ' 
+        RadioButton4.AutoSize = True
+        RadioButton4.Location = New Point(6, 97)
+        RadioButton4.Name = "RadioButton4"
+        RadioButton4.Size = New Size(70, 19)
+        RadioButton4.TabIndex = 6
+        RadioButton4.Text = "Warning"
+        RadioButton4.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton3
+        ' 
+        RadioButton3.AutoSize = True
+        RadioButton3.Location = New Point(6, 72)
+        RadioButton3.Name = "RadioButton3"
+        RadioButton3.Size = New Size(73, 19)
+        RadioButton3.TabIndex = 6
+        RadioButton3.Text = "Question"
+        RadioButton3.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(6, 47)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(88, 19)
+        RadioButton2.TabIndex = 1
+        RadioButton2.Text = "Information"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Checked = True
+        RadioButton1.Location = New Point(6, 22)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(62, 19)
+        RadioButton1.TabIndex = 0
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "Critical"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Controls.Add(RadioButton10)
+        GroupBox2.Controls.Add(RadioButton9)
+        GroupBox2.Controls.Add(RadioButton8)
+        GroupBox2.Controls.Add(RadioButton7)
+        GroupBox2.Controls.Add(RadioButton6)
+        GroupBox2.Controls.Add(RadioButton5)
+        GroupBox2.Location = New Point(151, 366)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(476, 121)
+        GroupBox2.TabIndex = 6
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "Buttons"' 
+        ' RadioButton10
+        ' 
+        RadioButton10.AutoSize = True
+        RadioButton10.Location = New Point(168, 47)
+        RadioButton10.Name = "RadioButton10"
+        RadioButton10.Size = New Size(102, 19)
+        RadioButton10.TabIndex = 5
+        RadioButton10.Text = "Retry + Cancel"
+        RadioButton10.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton9
+        ' 
+        RadioButton9.AutoSize = True
+        RadioButton9.Location = New Point(168, 22)
+        RadioButton9.Name = "RadioButton9"
+        RadioButton9.Size = New Size(122, 19)
+        RadioButton9.TabIndex = 4
+        RadioButton9.Text = "Yes + No + Cancel"
+        RadioButton9.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton8
+        ' 
+        RadioButton8.AutoSize = True
+        RadioButton8.Location = New Point(6, 96)
+        RadioButton8.Name = "RadioButton8"
+        RadioButton8.Size = New Size(72, 19)
+        RadioButton8.TabIndex = 3
+        RadioButton8.Text = "Yes + No"
+        RadioButton8.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton7
+        ' 
+        RadioButton7.AutoSize = True
+        RadioButton7.Location = New Point(6, 72)
+        RadioButton7.Name = "RadioButton7"
+        RadioButton7.Size = New Size(144, 19)
+        RadioButton7.TabIndex = 2
+        RadioButton7.Text = "Abort + Retry + Ignore"
+        RadioButton7.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton6
+        ' 
+        RadioButton6.AutoSize = True
+        RadioButton6.Location = New Point(6, 47)
+        RadioButton6.Name = "RadioButton6"
+        RadioButton6.Size = New Size(91, 19)
+        RadioButton6.TabIndex = 1
+        RadioButton6.Text = "OK + Cancel"
+        RadioButton6.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton5
+        ' 
+        RadioButton5.AutoSize = True
+        RadioButton5.Checked = True
+        RadioButton5.Location = New Point(6, 22)
+        RadioButton5.Name = "RadioButton5"
+        RadioButton5.Size = New Size(41, 19)
+        RadioButton5.TabIndex = 0
+        RadioButton5.TabStop = True
+        RadioButton5.Text = "OK"
+        RadioButton5.UseVisualStyleBackColor = True
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(516, 489)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(111, 23)
+        Button1.TabIndex = 7
+        Button1.Text = "Start me an error!"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(292, 492)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(190, 19)
+        CheckBox1.TabIndex = 8
+        CheckBox1.Text = "Hide the dialog before starting."
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' LinkLabel1
+        ' 
+        LinkLabel1.AutoSize = True
+        LinkLabel1.Location = New Point(11, 493)
+        LinkLabel1.Name = "LinkLabel1"
+        LinkLabel1.Size = New Size(130, 15)
+        LinkLabel1.TabIndex = 9
+        LinkLabel1.TabStop = True
+        LinkLabel1.Text = "Report missing buttons"' 
+        ' LinkLabel2
+        ' 
+        LinkLabel2.AutoSize = True
+        LinkLabel2.Location = New Point(147, 493)
+        LinkLabel2.Name = "LinkLabel2"
+        LinkLabel2.Size = New Size(139, 15)
+        LinkLabel2.TabIndex = 10
+        LinkLabel2.TabStop = True
+        LinkLabel2.Text = "Report bugs/suggestions"' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(639, 515)
+        Controls.Add(LinkLabel2)
+        Controls.Add(LinkLabel1)
+        Controls.Add(CheckBox1)
+        Controls.Add(Button1)
+        Controls.Add(GroupBox2)
+        Controls.Add(GroupBox1)
+        Controls.Add(Label3)
+        Controls.Add(TextBox2)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox1)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "Form1"
+        Text = "Error Creator"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents RadioButton9 As RadioButton
+    Friend WithEvents RadioButton8 As RadioButton
+    Friend WithEvents RadioButton7 As RadioButton
+    Friend WithEvents RadioButton6 As RadioButton
+    Friend WithEvents RadioButton5 As RadioButton
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents RadioButton10 As RadioButton
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents LinkLabel2 As LinkLabel
+End Class
---
+++ Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+    Dim vbIcon
+    Dim vbButtons
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If RadioButton1.Checked = True Then
+            vbIcon = vbCritical
+        ElseIf RadioButton2.Checked = True Then
+            vbIcon = vbInformation
+        ElseIf RadioButton3.Checked = True Then
+            vbIcon = vbQuestion
+        ElseIf RadioButton4.Checked = True Then
+            vbIcon = vbExclamation
+        End If
+        If RadioButton5.Checked = True Then
+            vbButtons = vbOKOnly
+        ElseIf RadioButton6.Checked = True Then
+            vbButtons = vbOKCancel
+        ElseIf RadioButton7.Checked = True Then
+            vbButtons = vbAbortRetryIgnore
+        ElseIf RadioButton8.Checked = True Then
+            vbButtons = vbYesNo
+        ElseIf RadioButton9.Checked = True Then
+            vbButtons = vbYesNoCancel
+        ElseIf RadioButton10.Checked = True Then
+            vbButtons = vbRetryCancel
+        End If
+        If CheckBox1.Checked = True Then
+            Me.Hide()
+        End If
+        MsgBox(TextBox2.Text, vbButtons + vbIcon, TextBox1.Text)
+        If CheckBox1.Checked = True Then
+            Me.Show()
+        End If
+    End Sub
+End Class
